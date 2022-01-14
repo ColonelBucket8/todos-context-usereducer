6ae2e8745c4b4d4bae2629416a7064d32cf74f81
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2022-01-15 01:34:08 +0800

    added useLocalStorageReducer

--- src/contexts/todos.context.jsx
+++ src/contexts/todos.context.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
+import { useLocalStorageReducer } from "../hooks/useLocalStorageReducer";
 import todoReducer from "../reducers/todo.reducer";
 
 const defaultTodos = [{ id: 1, task: "Pet monkey", completed: false }];
@@ -6,7 +7,11 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
 
   return (
     <TodosContext.Provider value={todos}>
